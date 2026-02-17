248e83018e6642e071661bb86422593681402de5
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2026-02-17 19:00:25 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('AWS lab running 🚀');
+});
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
